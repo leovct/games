f9bb20e5800494cf4da07bde97c63578faed452b
Léo Vincent <l009.vincent@gmail.com>
2020-09-16 11:50:20 +0200

    piggy: fix basic player movement by adding delta into the formula

--- piggy/Pig.gd
+++ piggy/Pig.gd
@@ -1,19 +1,19 @@
 extends Area2D
 
-export var SPEED: float = 3.5
+export var SPEED: float = 150
 # with export keyword, the variable can be modified using godot's ui
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		move(SPEED,0)
+		move(SPEED, 0, delta)
 	if Input.is_action_pressed("ui_left"):
-		move(-SPEED,0)
+		move(-SPEED, 0, delta)
 	if Input.is_action_pressed("ui_up"):
-		move(0,-SPEED)
+		move(0, -SPEED, delta)
 	if Input.is_action_pressed("ui_down"):
-		move(0,SPEED)
+		move(0, SPEED, delta)
 
-func move(xVelocity: float, yVelocity: float):
-	position.x += xVelocity
-	position.y += yVelocity
+func move(xVelocity: float, yVelocity: float, delta: float):
+	position.x += xVelocity * delta
+	position.y += yVelocity * delta
 	
