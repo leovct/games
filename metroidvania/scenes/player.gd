9f8e90a345af22028e779d23cd4ed6062cc7c01b
leovct <l009.vincent@gmail.com>
2020-09-26 16:51:58 +0200

    metroidvania: improve character's movements and physics

--- metroidvania/scenes/player.gd
+++ metroidvania/scenes/player.gd
@@ -3,21 +3,40 @@ extends KinematicBody2D
 export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
 export (float) var FRICTION = 0.25
+export (int) var GRAVITY = 200
+export (int) var JUMP_FORCE = 128
+export (int) var MAX_SLOPE_ANGLE = 46
 
 var motion = Vector2.ZERO
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var input_vector = get_input_vector()
+	apply_forces(input_vector, delta)
+	jump_check()
+	motion = move_and_slide(motion, Vector2.UP)
 
-	if input_vector != Vector2.ZERO:
-		motion += input_vector * ACCELERATION * delta
-		motion = motion.clamped(MAX_SPEED) # prevent from moving too fast
+func get_input_vector():
+	var vector = Vector2.ZERO
+	vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	return vector
+
+func apply_forces(input_vector, delta):
+	# apply horizontal force
+	if input_vector.x != 0:
+		motion.x += input_vector.x * ACCELERATION * delta
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	# apply friction
+	if input_vector.x == 0 && is_on_floor():
+		motion.x = lerp(motion.x, 0, FRICTION)
+	# apply gravity
+	#if not is_on_floor():
+	motion.y += GRAVITY * delta
+	motion.y = min(motion.y, JUMP_FORCE)
+
+func jump_check():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -JUMP_FORCE
 	else:
-		# every frame the player doesn't move, the motion is reduced by 25% (FRICTION)
-		motion = motion.linear_interpolate(Vector2.ZERO, FRICTION)
-	
-	motion = move_and_slide(motion) 
-
-
+		if Input.is_action_just_released("ui_up") && motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2
