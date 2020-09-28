ab9919efa7ff7aa3064363570f30ac42e45ddf4b
leovct <l009.vincent@gmail.com>
2020-09-28 20:03:19 +0200

    metroidvania: add player animations

--- metroidvania/scripts/player.gd
+++ metroidvania/scripts/player.gd
@@ -3,9 +3,12 @@ extends KinematicBody2D
 export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
 export (float) var FRICTION = 0.25
-export (int) var GRAVITY = 200
+export (int) var GRAVITY = 250
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
+
+onready var sprite = $Sprite
+onready var spriteAnimator = $AnimationPlayer
 
 var motion = Vector2.ZERO
 
@@ -13,6 +16,7 @@ func _physics_process(delta):
 	var input_vector = get_input_vector()
 	apply_forces(input_vector, delta)
 	jump_check()
+	update_animations(input_vector)
 	motion = move_and_slide(motion, Vector2.UP)
 
 func get_input_vector():
@@ -40,3 +44,13 @@ func jump_check():
 	else:
 		if Input.is_action_just_released("ui_up") && motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE/2
+
+func update_animations(input_vector):
+	if input_vector.x != 0:
+		sprite.scale.x = sign(input_vector.x)
+		spriteAnimator.play("Run")
+	else:
+		spriteAnimator.play("Idle")
+	
+	if not is_on_floor():
+		spriteAnimator.play("Jump")
