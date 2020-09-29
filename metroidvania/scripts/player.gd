f22ab1d63eeef8764a38b97cafb77e9c2a091052
leovct <l009.vincent@gmail.com>
2020-09-29 17:22:36 +0200

    metroidvania: fix slope movements

--- metroidvania/scripts/player.gd
+++ metroidvania/scripts/player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
 export (float) var FRICTION = 0.25
-export (int) var GRAVITY = 250
+export (int) var GRAVITY = 200
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 
@@ -11,13 +11,18 @@ onready var sprite = $Sprite
 onready var spriteAnimator = $AnimationPlayer
 
 var motion = Vector2.ZERO
+var snap_vector = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = get_input_vector()
 	apply_forces(input_vector, delta)
 	jump_check()
 	update_animations(input_vector)
-	motion = move_and_slide(motion, Vector2.UP)
+	var last_position = position
+	motion = move_and_slide_with_snap(motion, snap_vector, Vector2.UP, false, 4, deg2rad(MAX_SLOPE_ANGLE))
+	# small hack to prevent sliding on slopes
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 1:
+		position = last_position
 
 func get_input_vector():
 	var vector = Vector2.ZERO
@@ -33,17 +38,21 @@ func apply_forces(input_vector, delta):
 	if input_vector.x == 0 && is_on_floor():
 		motion.x = lerp(motion.x, 0, FRICTION)
 	# apply gravity
-	#if not is_on_floor():
-	motion.y += GRAVITY * delta
-	motion.y = min(motion.y, JUMP_FORCE)
+	if not is_on_floor():
+		motion.y += GRAVITY * delta
+		motion.y = min(motion.y, JUMP_FORCE)
 
 func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
+			snap_vector = Vector2.ZERO
 			motion.y = -JUMP_FORCE
 	else:
 		if Input.is_action_just_released("ui_up") && motion.y < -JUMP_FORCE/2:
+			snap_vector = Vector2.ZERO
 			motion.y = -JUMP_FORCE/2
+		else:
+			snap_vector = Vector2.DOWN*4
 
 func update_animations(input_vector):
 	if input_vector.x != 0:
