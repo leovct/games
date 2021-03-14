ed52b8b3687b8827b589ba2c2b8e6ad2aecbc45a
leovct <l009.vincent@gmail.com>
2021-03-15 00:47:00 +0100

    simulation-game: add sterling engine

---
+++ simulation-game/Entity.gd
@@ -0,0 +1,5 @@
+class_name Entity
+extends Node2D
+
+func _ready():
+	pass
---
+++ simulation-game/scripts/StirlingEngineEntity.gd
@@ -0,0 +1,14 @@
+extends Entity
+
+const BOOTUP_TIME := 6.0
+
+onready var animation_player := $AnimationPlayer
+onready var tween := $Tween
+onready var piston_shaft := $PistonShaft
+
+func _ready() -> void:
+	animation_player.play("Piston running")
+	# Tween controls the speed of the animation and the color of the piston shaft
+	tween.interpolate_property(animation_player, "playback_speed", 0, 1, BOOTUP_TIME)
+	tween.interpolate_property(piston_shaft, "modulate", Color.white, Color(0.5, 1, 0.5), BOOTUP_TIME)
+	tween.start()
