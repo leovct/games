bf1d9526178c2b51b1bf8836f7f427cf8c792e11
leovct <l009.vincent@gmail.com>
2021-03-14 23:44:37 +0100

    simulation-game: project and worl setup

---
+++ simulation-game/scripts/Player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+export var movement_speed := 200.0
+
+func _physics_process(_delta: float) -> void:
+	var direction := get_direction()
+	move_and_slide(direction * movement_speed)
+
+# get_direction() returns a normalized direction vector based on the key pressed by the player
+func get_direction() -> Vector2:
+	var horizontal_input := Input.get_action_strength("right") - Input.get_action_strength("left")
+	var vertical_input := Input.get_action_strength("down") - Input.get_action_strength("up")
+	return Vector2(horizontal_input * 2.0, vertical_input).normalized()
